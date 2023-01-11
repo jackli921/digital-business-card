f03f898e0d6b16f7a3b9223df488228dd40493b6
jackli921 <li.jack0707@gmail.com>
2023-01-10 20:42:04 -0800

    chore: update links

--- Components/Info.js
+++ Components/Info.js
@@ -6,10 +6,17 @@ export default function Info(){
           <img className ="profile-img" src="../profile_square.png" />
           <h1 className="name">Run Qi(Jack) Li</h1>
           <p className="title">Frontend Developer</p>
-          <a className="website" href="keeponflowing.com">keeponflowing.com</a>
+          <a className="website" href="https://www.keeponflowing.com">keeponflowing.com</a>
           <div className="btn-container">
-              <button className="btn-primary"><i class="fa-solid fa-envelope"></i> &nbsp;Email</button>
-              <button className="btn-primary linkedin"><i class="fa-brands fa-linkedin"></i>     &nbsp;LinkedIn</button>
+
+              <a href="mailto: abc@example.com" target="_blank" className="btn-primary">
+                <i class="fa-solid fa-envelope"></i> &nbsp;Email
+              </a>
+
+              <a herf="https://www.linkedin.com/in/jackli921/" target="_blank" className="btn-primary linkedin">
+                <i class="fa-brands fa-linkedin"></i> &nbsp;LinkedIn
+             </a>
+
           </div>
         </div>  
     )
