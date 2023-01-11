f5d9c1a013fa18a389bc025bd0d3ede312f9578a
jackli921 <li.jack0707@gmail.com>
2023-01-10 20:46:15 -0800

    chore: open link in new tab

--- Components/Info.js
+++ Components/Info.js
@@ -6,14 +6,14 @@ export default function Info(){
           <img className ="profile-img" src="../profile_square.png" />
           <h1 className="name">Run Qi(Jack) Li</h1>
           <p className="title">Frontend Developer</p>
-          <a className="website" href="https://www.keeponflowing.com">keeponflowing.com</a>
+          <a className="website"  target="_blank" href="https://www.keeponflowing.com">keeponflowing.com</a>
           <div className="btn-container">
 
-              <a href="mailto: abc@example.com" target="_blank" className="btn-primary">
+              <a href="#" target="_blank" className="btn-primary">
                 <i class="fa-solid fa-envelope"></i> &nbsp;Email
               </a>
 
-              <a herf="https://www.linkedin.com/in/jackli921/" target="_blank" className="btn-primary linkedin">
+              <a herf="https://www.linkedin.com/in/jackli921/" className="btn-primary linkedin">
                 <i class="fa-brands fa-linkedin"></i> &nbsp;LinkedIn
              </a>
 
